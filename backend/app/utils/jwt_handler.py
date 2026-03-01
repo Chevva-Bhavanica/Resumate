fb220a5486295449ed13ba14c2f42f3a3b76e6a8
Chevva Bhavanica <bhavanica.chevva@gmail.com>
2026-03-01 21:25:12 +0530

    Add JWT handler for token creation and decoding
    Implement JWT token creation and decoding functions.

---
+++ backend/app/utils/jwt_handler.py
@@ -0,0 +1,40 @@
+pip install python-jose
+# app/utils/jwt_handler.py
+
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from app.config import settings
+
+
+# --------------------------------------------------
+# Create Access Token
+# --------------------------------------------------
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
+
+
+# --------------------------------------------------
+# Decode Token
+# --------------------------------------------------
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM]
+        )
+        return payload
+    except JWTError:
+        return None
